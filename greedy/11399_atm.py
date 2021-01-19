200b213dcf80a218f7dbcf862b4e58e14c7cbbed
jimin3263 <dkdldsla0000@naver.com>
2021-01-19 20:47:27 +0900

    Update 11399_atm.py

--- greedy/11399_atm.py
+++ greedy/11399_atm.py
@@ -12,6 +12,7 @@ time.sort()
 # 1+3+6 == 최종 구하는 최소 시간
 
 '''
+#1. 가로로 계산
 min_sum=0 #전체 시간의 합
 i_sum=0 # 1+2+3 의 경우, 1+2까지의 값
 
@@ -19,10 +20,11 @@ for i in range(n):
     min_sum += (i_sum+time[i])
     i_sum = i_sum+time[i]
 '''
-min_sum=0 #전체 시간의 합
+
+#2. 세로로 계산
+# 1*3 + 2*2 + 3*1
+min_sum=0
 for i in range(n):
     min_sum += time[i]*(n-i)
-    
-
 
 print(min_sum)
