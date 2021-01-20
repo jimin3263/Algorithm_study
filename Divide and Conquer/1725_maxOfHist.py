2ee4ca838c4eea80ce04086c227f22827dbbb791
jimin3263 <dkdldsla0000@naver.com>
2021-01-20 22:04:20 +0900

    Create 1725_maxOfHist.py

---
+++ Divide and Conquer/1725_maxOfHist.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr=[]
+result=0
+for i in range(n):
+    arr.append(int(input()))
+arr.append(0)
+stack = [(0,arr[0])]
+
+for i in range(1,n+1):
+    stack_num = i
+    while(stack and arr[i]<stack[-1][1]):
+        stack_num,temp = stack.pop()
+        print(stack)
+        result = max(result,temp*(i-stack_num))
+    stack.append((stack_num,arr[i]))
+    
+print(result)  
