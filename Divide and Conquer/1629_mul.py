660a0f8748213ea10429e21bd2e41c75d49521aa
jimin3263 <dkdldsla0000@naver.com>
2021-01-20 23:25:24 +0900

    Update 1629_mul.py

--- Divide and Conquer/1629_mul.py
+++ Divide and Conquer/1629_mul.py
@@ -4,8 +4,10 @@ A, B, C = map(int,sys.stdin.readline().split())
 def divide_mul(a,b):
     if b==1:
         return a
+    #b가 홀수일 때
     elif b%2 ==1:
         return divide_mul(a,b-1)*a%C
+    #b가 짝수
     else:
         d=divide_mul(a,b//2)
         d%=C
