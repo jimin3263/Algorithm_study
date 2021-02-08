0addb7ebc4d648e51707a34718fdabd9d48c6b8a
jimin3263 <dkdldsla0000@naver.com>
2021-02-08 23:01:35 +0900

    Update 1197_Prim.py

--- MST/1197_Prim.py
+++ MST/1197_Prim.py
@@ -5,21 +5,22 @@ G=[[] for _ in range(V+1)]
 
 for i in range(E):
     a,b,c = map(int, sys.stdin.readline().split())
-    G[a].append((b,c))
-    G[b].append((a,c))
+    G[a].append((c,b))
+    G[b].append((c,a))
 
 def MST_prim():
     visited = [False]*(V+1)
-    q=[(1,0)]
+    q=[(0,1)]
     result = 0
     while q:
-        node,val=heappop(q)
+        val,node=heappop(q)
         if not visited[node]:
             visited[node]=True
             result += val
-            for ed,val in G[node]:
+            for val,ed in G[node]:
                 if not visited[ed]:
-                    heappush(q,(ed,val))
+                    heappush(q,(val,ed))
+                    print(q)
 
     return result
 print(MST_prim())
