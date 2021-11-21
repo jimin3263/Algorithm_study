e624b7cf704881a587e33746c4233304e22c1af1
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2021-11-22 02:15:52 +0900

    Create 11724.py

---
+++ Graph/11724.py
@@ -0,0 +1,25 @@
+#시작 2:04
+#n: 정점, m: 간선
+n,m=map(int,input().split())
+
+#인접 표시할 이차원 배열
+adj = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(m):
+    x,y = map(int,input().split())
+    adj[x][y],adj[y][x]=1,1
+
+visited_dfs = [False] * (n+1)
+def dfs(v):
+    visited_dfs[v] = True
+    for i in range(1,n+1):
+        if not visited_dfs[i] and adj[v][i] == 1:
+            dfs(i)
+
+cnt = 0
+for i in range(1, n+1):
+    if not visited_dfs[i]:
+        dfs(i)
+        cnt+=1
+
+print(cnt)
