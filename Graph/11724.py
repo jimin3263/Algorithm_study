315c605401c7fca1da8bc7df9b095c883833c50b
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2021-11-22 02:19:36 +0900

    Update 11724.py

--- Graph/11724.py
+++ Graph/11724.py
@@ -1,5 +1,7 @@
 #시작 2:04
 #n: 정점, m: 간선
+
+#dfs 
 n,m=map(int,input().split())
 
 #인접 표시할 이차원 배열
@@ -23,3 +25,35 @@ for i in range(1, n+1):
         cnt+=1
 
 print(cnt)
+
+#bfs
+'''
+#n: 정점, m: 간선
+n,m=map(int,input().split())
+
+#인접 표시할 이차원 배열
+adj = [[0]*(n+1) for _ in range(n+1)]
+
+for i in range(m):
+    x,y = map(int,input().split())
+    adj[x][y],adj[y][x]=1,1
+
+visited_bfs = [False] * (n+1)
+def bfs(v):
+    visited_bfs[v] = True
+    queue = [v]
+    while queue:
+        v= queue.pop(0)
+        for i in range(1,n+1):
+            if not visited_bfs[i] and adj[v][i] == 1:
+                queue.append(i)
+                visited_bfs[i] = True
+
+cnt = 0
+for i in range(1, n+1):
+    if not visited_bfs[i]:
+        bfs(i)
+        cnt+=1
+
+print(cnt)
+'''
