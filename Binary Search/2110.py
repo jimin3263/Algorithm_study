df8846aeea33bce4426068bddb0a4103b41d5084
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-01-10 17:15:33 +0900

    Update 2110.py

--- Binary Search/2110.py
+++ Binary Search/2110.py
@@ -20,15 +20,17 @@ while (start <= end):
     mid = (start+end) // 2 #중간 거리 값
     cur = arr[0] #현재 공유기 설치된 집
     cnt = 1 #공유기 설치 갯수
+    tmp = float("inf")
 
     for i in arr:
         if(cur + mid <= i): #중간 거리 값과 현재 위치보다 크거나 같은 거리가 있다면 공유기 설치
+            tmp = min(tmp, i-cur) #인접한 거리
             cnt += 1
             cur = i
     if (cnt < c): #설치한 공유기의 수가 목표한 수보다 작다면 왼쪽으로 옮긴다.
         end = mid - 1
     else:
-        result.append(mid)
+        result.append(tmp)
         start = mid + 1
 
 #print(result)
