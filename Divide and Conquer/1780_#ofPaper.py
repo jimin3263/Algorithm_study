ab9a78674243e625f92087411a53508c15364fbc
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-01-13 17:59:36 +0900

    Update 1780_#ofPaper.py

--- Divide and Conquer/1780_#ofPaper.py
+++ Divide and Conquer/1780_#ofPaper.py
@@ -34,3 +34,29 @@ divide(0,0,n)
 print(n_one)
 print(zero)
 print(one)
+
+##############
+# 결과값 딕셔너리로 저장 
+# n = int(input())
+# arr =[list(map(int,input().split())) for _ in range(n)]
+# 
+# result = {"-1": 0, "0": 0, "1": 0}
+# def divide(y,x,n):
+#     check = arr[y][x] #확인할 변수
+#     for i in range(y, y+n): #처음부터 n만큼 탐색
+#         for j in range(x, x+n):
+#             if arr[i][j] != check: #탐색 중 일치하지 않는 것이 있다면
+#                 n = n//3 #3으로 나눈다, 9개로 분리
+#                 for k in range(3):
+#                     for l in range(3):
+#                         divide(y+n*k,x+n*l,n)
+#                 return
+# 
+#     result[str(check)] += 1
+# 
+# 
+# #-1, 0, 1
+# divide(0,0,n)
+# print(result["-1"])
+# print(result["0"])
+# print(result["1"])
