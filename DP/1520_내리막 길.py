82c8b0a8e3af2ae9ac59dc229aaa82d38fa3d8ff
limjimin <dkdldsla0000@naver.com>
2022-05-24 16:50:48 +0900

    solved: BOJ 16234

---
+++ DP/1520_내리막 길.py
@@ -0,0 +1,28 @@
+#1520
+import sys
+input = sys.stdin.readline
+
+# m,n = map(int, input().split())
+# arr =[list(map(int,input().split())) for _ in range(m)]
+m, n = 4, 5
+arr = [[50, 45, 37, 32, 30], [35, 50, 40, 20, 25], [30, 30, 25, 17, 28], [27, 24, 22, 15, 10]]
+
+dy = [-1,1,0,0]
+dx = [0,0,-1,1]
+dp = [[-1]*n for _ in range(m)]
+
+def dfs(y, x):
+    if y == m-1 and x == n-1: #끝까지 도달한 경우
+        return 1
+    if dp[y][x] != -1:
+        return dp[y][x]
+    dp[y][x] = 0
+    for i in range(4):
+        ny, nx = dy[i]+y, dx[i]+x
+        if (0 <= ny < m and 0 <= nx < n and arr[y][x] > arr[ny][nx]):
+            dp[y][x] += dfs(ny, nx)
+    print(y,x, dp)
+    return dp[y][x]
+
+print(dfs(0,0))
+
